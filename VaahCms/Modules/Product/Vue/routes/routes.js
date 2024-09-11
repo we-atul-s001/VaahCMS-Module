36a154d129e396d18e833aa8d3054e7c6e871ea5
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-09-11 13:38:35 +0530

    Practice Taxonomy

--- VaahCms/Modules/Product/Vue/routes/routes.js
+++
@@ -1,9 +0,0 @@
-let routes= [];
-
-import dashboard from "./vue-routes-dashboard";
-import categories from "./vue-routes-categories";
-
-routes = routes.concat(dashboard);
-routes = routes.concat(categories);
-
-export default routes;
