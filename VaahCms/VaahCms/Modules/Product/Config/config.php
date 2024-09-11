3fc2e5f8ca924437f044c513c5891c60813ab841
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-09-11 18:19:47 +0530

    Practice: Change Footer Content

--- VaahCms/VaahCms/Modules/Product/Config/config.php
+++ VaahCms/VaahCms/Modules/Product/Config/config.php
@@ -11,7 +11,7 @@ return [
     "download_link"=> "",
     "author_name"=> "vaah",
     "author_website"=> "https://vaah.dev",
-    "version"=> "0.0.1",
+    "version"=> "1.0.0",
     "is_migratable"=> true,
     "is_sample_data_available"=> true,
     "db_table_prefix"=> "vh_product_",
